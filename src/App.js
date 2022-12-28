55975aec309c2551b0dddd4bce2f9ef6afa5d83e
syedacodes <syedacodes@gmail.com>
2022-12-28 22:54:20 +0530

    updating directory page with directory component and category item componenet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import Directory from './components/directory/directory.component';
 
-//Create a component
 const App = () => {
-    return  <h1>Hello World!</h1>
+    
+    const categories = [
+        { 
+            id: 1,
+            title: 'Hats',
+            imageURL: 'https://i.ibb.co/cvpntL1/hats.png'
+        },
+        { 
+            id: 2,
+            title: 'Jackets',
+            imageURL: 'https://i.ibb.co/px2tCc3/jackets.png'
+        },
+        { 
+            id: 3,
+            title: 'Sneakers',
+            imageURL: 'https://i.ibb.co/0jqHpnp/sneakers.png'
+        },
+        {
+            id: 4,
+            title: 'Womens',
+            imageURL: 'https://i.ibb.co/GCCdy8t/womens.png'
+        },
+        { 
+            id: 5,
+            title: 'Mens',
+            imageURL: 'https://i.ibb.co/R70vBrQ/men.png'
+        }
+    ]
+
+    return(
+        <Directory categories={categories}/>
+    )
 }
 
 export default App;
