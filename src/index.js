b760722307ff3c13a99500e10868529c7391df34
syedacodes <syedacodes@gmail.com>
2022-12-28 19:56:20 +0530

    import react and reactDOM

---
+++ src/index.js
@@ -0,0 +1,17 @@
+//Import the React and ReactDOM Libraries
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+//Get a reference to the div with ID root
+const el = document.getElementById('root');
+
+//Tell React to take control of that element
+const root  = ReactDOM.createRoot(el);
+
+//Create a component
+function App () {
+  return <h1>Hello World!</h1>;
+}
+
+//Show the component on the screen
+root.render(<App />);
